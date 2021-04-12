75f3d6dcf357340c899bdd37e9c1d03f75ec5e49
parixitas <sakhiya.parixita@gmail.com>
2021-04-12 09:58:22 +0530

    Asp.net Core MVC

---
+++ CoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,126 @@
+﻿using CoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly EmpContext _context;
+
+        public HomeController(EmpContext context, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        
+
+        public IActionResult Index()
+        {
+            return View(_context.Employee.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employee.Add(emp);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(emp);
+        }
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("Index");
+            }
+            Emp emp = _context.Employee.Find(id);
+            if (emp == null)
+            {
+                return View("Index");
+            }
+            return View(emp);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return View("Index");
+            }
+            Emp emp = _context.Employee.Find(id);
+            if (emp == null)
+            {
+                return View("Index");
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            if (id != 0)
+            {
+                Emp emp = _context.Employee.Find(id);
+                _context.Employee.Remove(emp);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View("Index");
+            }
+            Emp emp = _context.Employee.Find(id);
+            if (emp == null)
+            {
+                return View("Index");
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(emp).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoreMVC/Models/Emp.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Models
+{
+    [Table("Emp")]
+    public class Emp
+    {
+        [Key()]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
+        public string Email { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "age must be between 1 and 100")]
+        public int? Age { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+
+    public class EmpContext : DbContext
+    {
+        public EmpContext(DbContextOptions<EmpContext> options): base(options)
+        { 
+        }
+        public virtual DbSet<Emp> Employee { get; set; }
+    }
+}
